ce5ad12adb68818b5bb69b9c6d65b546c4655de3
Luke Schlessinger <lmschless@gmail.com>
2020-08-24 17:31:26 -0700

    have working routes and autofilled from templates. broke out replaceTemplate function into a separate module. Working on creating package.json and adding packages to the project.

--- starter/index.js
+++ starter/index.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import http from 'http';
 import url from 'url';
 import path from 'path';
-
+import replaceTemplate from './modules/replaceTemplate.js';
 const __dirname = path.resolve();
 //////////////////////////////////
 //// FILES
@@ -33,23 +33,53 @@ const __dirname = path.resolve();
 //// need to save createServer to a variable to call it later.
 
 // use ${__dirname} instead of ./ because it's based on wherever index.js is run. This makes it more dynamic.
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
+  const { query, pathname } = url.parse(req.url, true);
 
-  const pathName = req.url;
-  if (pathName === '/overview') {
-    res.end('This is the overview!');
-  } else if (pathName === '/product') {
-    res.end('This is the product!');
-  } else if (pathName === '/api') {
+  // const pathName = req.url;
+  // Overview
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    const cardsHtml = dataObj
+      .map((card) => replaceTemplate(tempCard, card))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    // console.log(cardsHtml);
+    res.end(output);
+    // Product
+  } else if (pathname === '/product') {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+    // API
+  } else if (pathname === '/api') {
     res.writeHead(404, {
       'Content-type': 'text/html',
       'my-own-header': 'hello-world',
     });
-    res.end(data);
+    res.end(output);
+    // Not Found
   } else {
     // writeHead sends a status code and a header
     // to tell the browser what to expect
@@ -58,6 +88,7 @@ const server = http.createServer((req, res) => {
       'Content-type': 'text/html',
       'my-own-header': 'hello-world',
     });
+    res.end('Page not found!');
   }
 });
 
